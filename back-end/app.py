898334b587cb8117dc0250135f69a0530e68633b
kurachi <matheuskurachi6@gmail.com>
2026-05-28 08:15:49 -0300

    Refactor app.py: improve formatting and organization of import and configuration sections

--- back-end/app.py
+++ back-end/app.py
@@ -1,16 +1,26 @@
+#                            IMPORT DAS BIBLIOTECAS                                     #
+
 import os
 import time
 
 try:
     import paho.mqtt.client as mqtt
+
 except ImportError:
     os.system("cls")
     print("Problema na importação da Biblioteca MQTT")
     time.sleep(5)
+
+#                            IMPORT DAS BIBLIOTECAS                                     #
+
+
+#                            CONFIGURAÇÃO INICIAL                                       #
 
 BROKER = "" # Coloque o IP DO BROKER.
 PORT = "1833" # Coloque a porta que o BROKER está (PADRÃO = 1833)
 TOPIC = "" # Coloque o nome do tópico
 USERNAME = "grupo1" # Coloque o usuário do Projeto
 PASSWORD = "grupo1" # Coloque a senha do Projeto
+
+#                            CONFIGURAÇÃO INICIAL                                       #
 
