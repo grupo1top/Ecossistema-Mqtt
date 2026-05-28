9844a92a2e3239e62b43007014da6c6c90c32b33
kurachi <matheuskurachi6@gmail.com>
2026-05-28 08:26:50 -0300

    Remove commented initial configuration section from app.py

--- back-end/app.py
+++ back-end/app.py
@@ -10,17 +10,3 @@ except ImportError:
     os.system("cls")
     print("Problema na importação da Biblioteca MQTT")
     time.sleep(5)
-
-#                            IMPORT DAS BIBLIOTECAS                                     #
-
-
-#                            CONFIGURAÇÃO INICIAL                                       #
-
-BROKER = "" # Coloque o IP DO BROKER.
-PORT = "1833" # Coloque a porta que o BROKER está (PADRÃO = 1833)
-TOPIC = "" # Coloque o nome do tópico
-USERNAME = "grupo1" # Coloque o usuário do Projeto
-PASSWORD = "grupo1" # Coloque a senha do Projeto
-
-#                            CONFIGURAÇÃO INICIAL                                       #
-
