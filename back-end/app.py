5a0db6150a3d30133b32feb0595bd90a42440312
kurachi <matheuskurachi6@gmail.com>
2026-05-28 08:09:40 -0300

    Add MQTT connection parameters to app.py

--- back-end/app.py
+++ back-end/app.py
@@ -8,5 +8,9 @@ except ImportError:
     print("Problema na importação da Biblioteca MQTT")
     time.sleep(5)
 
-
+BROKER = "" # Coloque o IP DO BROKER.
+PORT = "1833" # Coloque a porta que o BROKER está (PADRÃO = 1833)
+TOPIC = "" # Coloque o nome do tópico
+USERNAME = "grupo1" # Coloque o usuário do Projeto
+PASSWORD = "grupo1" # Coloque a senha do Projeto
 
